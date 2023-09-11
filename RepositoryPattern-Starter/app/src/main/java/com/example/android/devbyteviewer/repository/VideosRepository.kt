d46001a258857cbeb72c6603efd1c59660cd4214
Naimjr10 <njr848262@gmail.com>
2023-09-11 10:47:33 +0700

    Update VideosRepository.kt
    - Add a repository
    - Retrieve data from the database

--- RepositoryPattern-Starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ RepositoryPattern-Starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,7 +16,42 @@
 
 package com.example.android.devbyteviewer.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.DevByteVideo
+import com.example.android.devbyteviewer.network.DevByteNetwork
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 /**
  * Repository for fetching devbyte videos from the network and storing them on disk
  */
-// TODO: Implement the VideosRepository class
+class VideosRepository(private val database: VideosDatabase) {
+
+    // Because the getVideos() method returns a list of database objects,
+    // and not a list of DevByteVideo objects,
+    // use Transformations.map to convert the list of database objects to
+    // a list of domain objects using the asDomainModel() conversion function.
+    val videos: LiveData<List<DevByteVideo>> = Transformations.map(
+        database.videoDao.getVideos() ) { it.asDomainModel() }
+
+    /*
+     * Databases on Android are stored on the file system, or disk, and
+     * in order to save they must perform a disk I/O operation.
+     * Disk I/O, or reading and writing to disk, is slow and always blocks
+     * the current thread until the operation is complete.
+     * Because of this, you have to run the disk I/O in the I/O dispatcher.
+     * This dispatcher is designed to offload blocking I/O tasks to
+     * a shared pool of threads using withContext(Dispatchers.IO) { ... }.
+     */
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = DevByteNetwork.devbytes.getPlaylist()
+            database.videoDao.insertAll( playlist.asDatabaseModel() )
+        }
+    }
+
+}
